483a9e097f6f506222b537f1b19bde9938f16462
Jordan Burger <jordanburger22@gmail.com>
2023-11-20 11:45:39 -0700

    added code to server.js for deploy

--- server.js
+++ server.js
@@ -4,24 +4,39 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 require('dotenv').config()
 const { expressjwt } = require('express-jwt')
+const path = require('path')
 
-mongoose.connect(process.env.MONGOURI, console.log('Connected to DB'))
+async function connectToDB(){
+    try{
+        await mongoose.connect(process.env.MONGOURI)
+        console.log('connected to db')
+    } catch(err){
+        console.log('mongoose connection err: ', err)
+    }
+}
+
+connectToDB()
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.use('/api/auth', require('./routes/userRouter'))
-app.use('/api/main', expressjwt({secret: process.env.SECRET, algorithms: ['HS256']}))
+app.use('/api/main', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] }))
 app.use('/api/main/todos', require('./routes/todoRouter'))
 
 app.use((err, req, res, next) => {
     console.log(err)
-    if(err.name === "UnauthorizedError"){
+    if (err.name === "UnauthorizedError") {
         res.status(err.status)
     }
-    return res.send({errMsg: err.message})
+    return res.send({ errMsg: err.message })
 })
 
-app.listen(7550, () => {
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(7551, () => {
     console.log('server is running on port 7550')
 })
